588b769823a65825dbc4a78976e7859cbe2ce0e8
dchou1618 <dchou5217@gmail.com>
2025-12-20 00:38:54 -0500

    add utils pyspark
    vector dot product

---
+++ tests/test_vector.py
@@ -0,0 +1,27 @@
+import pytest
+from pyspark.sql import SparkSession, functions as F
+from utils.spark.vector import dot_product
+
+@pytest.fixture(scope="module")
+def spark():
+    spark_session = SparkSession.builder \
+        .appName("DotProductTest") \
+        .master("local[*]") \
+        .getOrCreate()
+    yield spark_session
+    spark_session.stop()
+
+def test_dot_product(spark):
+    data = [
+        ([1.0, 2.0, 3.0], [4.0, 5.0, 6.0]),
+        ([0.0, 1.0, 0.0], [1.0, 2.0, 3.0]),
+        ([2.0, 0.0, 1.0], [0.0, 1.0, 0.0]),
+    ]
+    df = spark.createDataFrame(data, schema=["vec1", "vec2"])
+
+    df = df.withColumn("dot_prod", dot_product(F.col("vec1"), F.col("vec2")))
+
+    results = df.select("dot_prod").rdd.flatMap(lambda x: x).collect()
+
+    expected = [32.0, 2.0, 0.0]
+    assert results == expected
---
+++ utils/spark/vector.py
@@ -0,0 +1,14 @@
+from pyspark.sql import functions as F
+from pyspark.sql import DataFrame
+from pyspark.sql import Column
+
+def dot_product(col1: Column, 
+                col2: Column):
+    """
+    Compute dot product between two vector columns
+    """
+    return F.aggregate(
+        F.arrays_zip(col1.alias("v1"), col2.alias("v2")),
+        F.lit(0.0),
+        lambda acc, x: acc + x.v1 * x.v2
+    )
