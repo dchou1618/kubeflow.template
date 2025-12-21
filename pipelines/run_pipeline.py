67e9b057af11fe37490a46746b6351a222ea2556
dchou1618 <dchou5217@gmail.com>
2025-12-21 00:45:00 -0500

    update with host and auth
    currently debugging auth, but to compile and test basic kubeflow workflow

---
+++ pipelines/run_pipeline.py
@@ -0,0 +1,19 @@
+import kfp
+import os
+
+client = kfp.Client(host=os.environ['KFP_HOST'], existing_token=os.environ['KFP_AUTH_TOKEN'])
+
+# upload or use existing pipeline
+experiment = client.create_experiment('experiment')
+
+run = client.run_pipeline(
+    experiment_id=experiment.id,
+    job_name='pipeline-run',
+    pipeline_package_path='pipelines/logistic_regression_pipeline.json',
+    params={
+        'n_samples': 1000,
+        'test_size': 0.2,
+        'weights': [0.5, -0.2, 0.3]
+    },
+)
+print(f"Run started: {run.id}")
